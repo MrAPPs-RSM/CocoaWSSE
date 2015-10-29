bd089647384506283c795b3dbdb1e9186b175ab0
Nicola Innocenti <nicola.innocenti9@gmail.com>
2015-10-29 10:43:30 +0100

    Fix 2.0

--- CocoaWSSE/CocoaWSSE.m
+++ CocoaWSSE/CocoaWSSE.m
@@ -61,26 +61,8 @@
   //[dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"US"]];
   [dateFormatter setTimeZone:[NSTimeZone timeZoneWithName:@"UTC"]];
   [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
-  
-  NSString *stringDate = [dateFormatter stringFromDate:now];
-  NSString *lastSecurityDate = [[NSUserDefaults standardUserDefaults] objectForKey:@"lastSecurityDate"];
-  if(!lastSecurityDate){
-      [[NSUserDefaults standardUserDefaults] setObject:stringDate forKey:@"lastSecurityDate"];
-      [[NSUserDefaults standardUserDefaults] synchronize];
-  }
-  else
-  {
-      NSDate *lastDate = [dateFormatter dateFromString:lastSecurityDate];
-      NSDate *currentDate = [dateFormatter dateFromString:stringDate];
-      if([currentDate timeIntervalSinceDate:lastDate] > 250){
-          [[NSUserDefaults standardUserDefaults] setObject:stringDate forKey:@"lastSecurityDate"];
-          [[NSUserDefaults standardUserDefaults] synchronize];
-      }
-      else
-          stringDate = lastSecurityDate;
-  }
     
-  return stringDate;
+  return [dateFormatter stringFromDate:now];
 }
 
 @end
