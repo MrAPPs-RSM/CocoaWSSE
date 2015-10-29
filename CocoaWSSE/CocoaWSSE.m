c61adec59d089d5e33b4da62a39b443f7c9fcebb
Nicola Innocenti <nicola.innocenti9@gmail.com>
2015-10-29 10:42:46 +0100

    Fix verifica vecchia security

--- CocoaWSSE/CocoaWSSE.m
+++ CocoaWSSE/CocoaWSSE.m
@@ -18,10 +18,36 @@
 	NSString *passwordDigest = [[CocoaSecurity sha1:[NSString stringWithFormat:@"%@%@%@", nonce, formattedDate, password]] base64];
 	NSString *base64 = [[CocoaSecurityEncoder new] base64:[nonce dataUsingEncoding:NSASCIIStringEncoding]];
 	NSString *credentials = [NSString stringWithFormat:
-                           @"UsernameToken Username=\"%@\", "
-                           @"PasswordDigest=\"%@\", "
-                           @"Nonce=\"%@\", "
-                           @"Created=\"%@\"", username, passwordDigest, base64, formattedDate];
+                             @"UsernameToken Username=\"%@\", "
+                             @"PasswordDigest=\"%@\", "
+                             @"Nonce=\"%@\", "
+                             @"Created=\"%@\"", username, passwordDigest, base64, formattedDate];
+    
+    NSString *lastSecurityDate = [[NSUserDefaults standardUserDefaults] objectForKey:@"lastSecurityDate"];
+    if(!lastSecurityDate){
+        
+        [[NSUserDefaults standardUserDefaults] setObject:formattedDate forKey:@"lastSecurityDate"];
+        [[NSUserDefaults standardUserDefaults] setObject:credentials forKey:@"lastSecurity"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+    else
+    {
+        NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setTimeZone:[NSTimeZone timeZoneWithName:@"UTC"]];
+        [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];
+        NSDate *lastDate = [dateFormatter dateFromString:lastSecurityDate];
+        NSDate *currentDate = [dateFormatter dateFromString:formattedDate];
+        
+        if([currentDate timeIntervalSinceDate:lastDate] > 250){
+            
+            [[NSUserDefaults standardUserDefaults] setObject:formattedDate forKey:@"lastSecurityDate"];
+            [[NSUserDefaults standardUserDefaults] setObject:credentials forKey:@"lastSecurity"];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+        }
+        else
+            credentials = [[NSUserDefaults standardUserDefaults] objectForKey:@"lastSecurity"];
+    }
+    
 	return credentials;
 }
 
